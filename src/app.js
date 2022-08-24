454a9156f314d5ad24aeeb389961a3f458db1165
eOrbis <108908217+eOrbis@users.noreply.github.com>
2022-08-24 16:44:39 -0700

    Removed console.logs.

--- src/app.js
+++ src/app.js
@@ -19,7 +19,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#current-temp");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#current-description");
@@ -37,7 +36,5 @@ function displayTemperature(response) {
 
 let apiKey = "eee5ee31d243367da9788e9b7ef6d18f";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=${apiKey}&units=metric`;
-
-console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
